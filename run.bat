7718dff2f752480d9cff43c518c76349c6febb42
Saagar <kamranisaagar@gmail.com>
2017-03-17 22:48:27 +1100

    ui implemented

--- run.bat
+++ run.bat
@@ -1,10 +1,3 @@
 Taskkill /IM javaw.exe /F
 
-echo "Setting New Config"
-cd configset
-call "newConfig.bat"
-
-echo "Post Cash and Transactions"
-cd ../postcash
-call "prompt.bat"
-
+java -jar ui.jar
