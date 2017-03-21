31f5147ba4b71194f8ee03dfae6c70c008dcf307
Saagar <kamranisaagar@gmail.com>
2017-03-21 13:05:22 +1100

    fix

--- postcash/postCash.bat
+++ postcash/postCash.bat
@@ -1,10 +1,14 @@
 cd C:/mpulse/scripts/postcash/
 
+echo "Deleting Logs"
+del /F /Q "C:\mpulse\scripts\postcash\logCC.txt"
+del /F /Q "C:\mpulse\scripts\postcash\logPOST.txt"
+
 echo "Performing Close Cash"
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" doCloseCash.php >> log.txt
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" doCloseCash.php >> logCC.txt
 
 echo "Post Cash and Transactions"
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" postTransactions.php >> log.txt
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" postTransactions.php >> logPOST.txt
 
 exit
 
