56036ac26f5e1bc9421d99c32daa6b8cac15bdcb
Saagar Kamrani <kamranisaagar@gmail.com>
2017-05-19 16:21:54 +1000

    cc option revert

--- postcash/postCash.bat
+++ postcash/postCash.bat
@@ -1,7 +1,7 @@
 cd C:/mpulse/scripts/postcash/
 
 echo "Performing Close Cash"
-rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" doCloseCash.php
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" doCloseCash.php
 
 echo "Post Cash and Transactions"
 "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" postTransactions.php
