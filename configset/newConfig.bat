571df7d3a36ec31af456a0be2f8210b7d81a9b5b
Saagar Kamrani <kamranisaagar@gmail.com>
2017-05-19 14:14:07 +1000

    copy unicenta.jar

--- configset/newConfig.bat
+++ configset/newConfig.bat
@@ -1,4 +1,6 @@
 echo "Setting Configuration"
+del C:/Unicenta/unicenta.jar
 cd C:/mpulse/scripts/configset/
 "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
+copy unicenta.jar "C:/Unicenta/unicenta.jar"
 exit
