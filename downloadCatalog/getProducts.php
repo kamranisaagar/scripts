e9f3d9eee458632b8fad8645c87d0ba4a0bb78c0
Saagar Kamrani <kamranisaagar@gmail.com>
2017-05-24 02:02:42 +1000

    download Catalog Test

---
+++ downloadCatalog/getProducts.php
@@ -0,0 +1,38 @@
+<?php
+
+$storeinfo = parse_ini_file("../../storeinfo.ini");
+
+$storeid=$storeinfo['storeid'];
+
+//Get Products
+
+$query = "SELECT CONCAT('ON',p.promoid) as promoid, promoname, DATE_FORMAT(startdate,'%Y%m%d') as startdate, 
+DATE_FORMAT(enddate,'%Y%m%d') as enddate, '0', '24', NULL, subcat, '2', amount, NULL, NULL,
+ NULL, NULL, NULL, NULL, ctnamount, REPLACE(REPLACE(disabled, '1', 'RECALLED_ACK'),'0','ACKNOWLEDGED') as remote , '0' FROM promotions p
+
+JOIN promostore ps ON p.promoid=ps.promoid AND ps.storeid={$storeid}";
+			  
+$result = $link2->query($query) or die("Error in the consult.." . mysqli_error($link2));
+
+while ($row = mysqli_fetch_assoc($result)) {	
+	
+	$fields=array();
+
+	foreach($row as $field) {
+		$fields[]=$field;
+	}
+
+	$val[]=implode(",",$fields);
+
+}
+
+$values = implode(",", $val);
+
+$query = "INSERT IGNORE INTO promo_header values {$values}";
+
+echo $query;
+
+//$result = $link->query($query) or die("Error in the consult.." . mysqli_error($link));	
+
+
+// Get Promotions
---
+++ downloadCatalog/getPromotions.php
@@ -0,0 +1,18 @@
+<?php
+
+$query = "SELECT * FROM supplierpurchasedata";
+			  
+$result = $link->query($query) or die("Error in the consult.." . mysqli_error($link));
+
+$values = "(0,0,0,0,0,0,0)\r\n";
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+	$values .= ", (\"{$row['ID']}\", {$storeid}, \"{$row['SUPPLIER_ID']}\", \"{$row['PURCHASEDATE']}\", \"{$row['STICKS']}\", \"{$row['PURCHASEMONTH']}\", \"{$row['PURCHASEYEAR']}\")\r\n";
+
+	}
+
+$query = "insert ignore into tsg_purchaseData values {$values}";
+
+$result = $link2->query($query) or die("Error in the consult.." . mysqli_error($link2));	
+
