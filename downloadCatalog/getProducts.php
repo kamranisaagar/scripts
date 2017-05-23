66cedc3a37ad8659426de91df0f9a6936e2eec5e
Saagar Kamrani <kamranisaagar@gmail.com>
2017-05-24 02:18:27 +1000

    Dcatalog fix

--- downloadCatalog/getProducts.php
+++ downloadCatalog/getProducts.php
@@ -9,8 +9,7 @@ $storeid=$storeinfo['storeid'];
 //Get Products
 
 $query = "SELECT CONCAT('ON',p.promoid) as promoid, promoname, DATE_FORMAT(startdate,'%Y%m%d') as startdate, 
-DATE_FORMAT(enddate,'%Y%m%d') as enddate, '0', '24', NULL, subcat, '2', amount, NULL, NULL,
- NULL, NULL, NULL, NULL, ctnamount, REPLACE(REPLACE(disabled, '1', 'RECALLED_ACK'),'0','ACKNOWLEDGED') as remote , '0' FROM promotions p
+DATE_FORMAT(enddate,'%Y%m%d') as enddate, '0', '24', subcat, '2', amount, ctnamount, REPLACE(REPLACE(disabled, '1', 'RECALLED_ACK'),'0','ACKNOWLEDGED') as remote , '0' FROM promotions p
 
 JOIN promostore ps ON p.promoid=ps.promoid AND ps.storeid={$storeid}";
 			  
@@ -24,13 +23,13 @@ while ($row = mysqli_fetch_assoc($result)) {
 		$fields[]=$field;
 	}
 
-	$val[]="(".implode(",",$fields).")";
+	$val[]="(".implode("','",$fields).")";
 
 }
 
 $values = implode(",", $val);
 
-$query = "INSERT IGNORE INTO promo_header values {$values}";
+$query = "INSERT IGNORE INTO promo_header(id,name,startdate,enddate,starthour,endhour,articlecategory,type,amount,ctnamount,remote,markedexpired) values {$values}";
 
 echo $query;
 
