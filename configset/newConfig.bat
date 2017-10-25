f164990770d83426128be43a46630b95ef0de096
Saagar <kamranisaagar@gmail.com>
2017-10-26 02:56:12 +1100

    server changes

--- configset/newConfig.bat
+++ configset/newConfig.bat
@@ -1,8 +1,8 @@
 Taskkill /IM javaw.exe /F
 
-del C:/mpulse/assets/db.php
+del "C:\mpulse\assets\db.php"
 
-copy C:/mpulse/scripts/temp/db.php C:/mpulse/assets/db.php
+copy "C:\mpulse\scripts\temp\db.php" "C:\mpulse\assets\db.php"
 
 echo "Setting Properties"
 
@@ -13,5 +13,6 @@ cd C:/mpulse/scripts/configset/
 "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
 
 "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
+
 
 exit
