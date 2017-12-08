7417778d0e08f5a665348f30970929e3eef96ad1
Saagar <kamranisaagar@gmail.com>
2017-12-08 16:19:49 +1100

    scheduler implement

--- configset/newConfig.bat
+++ configset/newConfig.bat
@@ -2,13 +2,14 @@ Taskkill /IM javaw.exe /F
 
 echo "Setting Properties"
 
-cd C:/mpulse/scripts/configset/
+-- cd C:/mpulse/scripts/configset/
 
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setProperties.php
+-- "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setProperties.php
 
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
+-- "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
 
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
+-- "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
 
+schtasks /create /tn "MPulseSendTrans" /tr C:/mpulse/scripts/postcash/sendTrans.bat /sc minute /mo 60 /st 09:40
 
 exit
