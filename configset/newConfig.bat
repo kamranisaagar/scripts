bb7f6c5542d4a38b24e5f02313af26f3ea7cff04
Saagar <kamranisaagar@gmail.com>
2017-12-16 09:24:59 +1100

    properties

--- configset/newConfig.bat
+++ configset/newConfig.bat
@@ -4,9 +4,9 @@ echo "Setting Properties"
 
 cd C:/mpulse/scripts/configset/
 
-rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setProperties.php
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setProperties.php
 
-rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
 
 rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
 
