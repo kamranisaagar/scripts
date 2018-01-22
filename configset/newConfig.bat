21d36914abbff0d98c2207f682c6aa606de8996b
Saagar <kamranisaagar@gmail.com>
2018-01-22 14:52:20 +1100

    set perc

--- configset/newConfig.bat
+++ configset/newConfig.bat
@@ -6,8 +6,8 @@ cd C:/mpulse/scripts/configset/
 
 rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setProperties.php
 
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
+rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setConfig.php
 
-rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
 
 exit
