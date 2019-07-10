0c084a3321b3c738e714f4fa9d5c77e7fa70b914
kamranisaagar <kamranisaagar@gmail.com>
2019-07-11 00:34:14 +1000

    Update catalog.bat

--- downloadCatalog/catalog.bat
+++ downloadCatalog/catalog.bat
@@ -1,7 +1,7 @@
 cd C:/mpulse/scripts/downloadCatalog/
 
 echo "Upload Prices"
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" getPrices.php.php >> log.txt
+"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" getPrices.php >> log.txt
 
 echo "Downloading Catalog"
 "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" getCatalog.php >> log.txt
