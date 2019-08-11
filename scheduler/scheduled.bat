9f5cbbd86c942b5fe9b650646e52bd3356ce6ca8
kamranisaagar <kamranisaagar@gmail.com>
2019-08-11 17:16:15 +1000

    Update scheduled.bat

--- scheduler/scheduled.bat
+++ scheduler/scheduled.bat
@@ -1,14 +1,8 @@
-cd C:/mpulse/scripts/scheduler/
-
-echo "Executing Scheduler"
-
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" execute.php
-
 cd C:/mpulse/scripts/downloadCatalog/
 
 echo "Downloading Catalog"
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" getCatalog.php
-"C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
+rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" getCatalog.php
+rem "C:\mpulse\assets\php_compiler\php5.4.16\php.exe" setPercentages.php
 
 cd C:/mpulse/scripts/stockScan/
 
